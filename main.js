a235510304d577ee7a6a7505a5368a4884ed46d8
Enderna <adryanender@gmail.com>
2025-08-20 14:41:32 +0200

    Deleted alert and colorings Added todos for future improvments

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const CURRENCY = "zł";
 
+// TODO: When searching for original price, add exception cus sometimes it writes "FREE"
+// TODO: Should categorize percentages to losing money and gaining money ( 0 also )
+    // MAYBE: colorise items to green, red and gradient for ex.
+// MAYBE: change from odds to range ( could be more precise )
+
 const main = ()=>{
     const list_items = document.querySelectorAll(`
         .ContainerGroupedItem >
@@ -8,13 +13,10 @@ const main = ()=>{
         tbody >
         .table_row
     `);
-
-    alert(list_items.length);
     
     let avg_price = 0;
 
     list_items.forEach(item => {
-        item.style.backgroundColor = 'red';
         let [_, price, __, odds] = [...item.querySelectorAll('td')].map(child => child.textContent.trim());
 
         avg_price += parseFloat(price)*parseFloat(odds);
@@ -30,7 +32,7 @@ const main = ()=>{
 
         const span_percent = document.createElement('span');
         const percents = Math.round((avg_price / original_price) * 100);
-        
+
         span_percent.textContent = ` (${percents}%)`;
         span_percent.style.color = percents > 100 ? "green" : "red";
 
