32a33545dcfe5dd72b2368aedbbd22baa6c6e074
Enderna <adryanender@gmail.com>
2025-08-20 15:59:42 +0200

    Made odds for losing and gaining money

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const CURRENCY = "zł";
+const CURRENCY = document.cookie.split('; ')
+    .map(cookie => cookie.trim())
+    .find(cookie => cookie.startsWith('currency='))
+    ?.split('=')[1] || 'USD';
 
 // TODO: When searching for original price, add exception cus sometimes it writes "FREE"
-// TODO: Should categorize percentages to losing money and gaining money ( 0 also )
-    // MAYBE: colorise items to green, red and gradient for ex.
+// MAYBE: colorise items to green, red and gradient for ex.
 // MAYBE: change from odds to range ( could be more precise )
 
 const main = ()=>{
@@ -13,30 +15,43 @@ const main = ()=>{
         tbody >
         .table_row
     `);
+
+    const price_label = document.querySelector('.ContainerPrice > .Currency');
+    const original_price = parseFloat(price_label.textContent.trim());
     
-    let avg_price = 0;
+    let avg_price = 0,
+        odds_to_gain = 0,   // Gain or make 0
+        odds_to_lose = 0;
 
     list_items.forEach(item => {
         let [_, price, __, odds] = [...item.querySelectorAll('td')].map(child => child.textContent.trim());
 
-        avg_price += parseFloat(price)*parseFloat(odds);
+        price = parseFloat(price);
+        odds = parseFloat(odds);
+
+        if(price < original_price) odds_to_lose += odds;
+        else odds_to_gain += odds;
+
+        avg_price += price * odds;
     });
 
     avg_price = Math.round(avg_price) / 100;
-
-    const price_label = document.querySelector('.ContainerPrice > .Currency');
-    const original_price = parseFloat(price_label.textContent.trim());
+    odds_to_gain = Math.round(odds_to_gain);
+    odds_to_lose = Math.round(odds_to_lose);
 
     setTimeout(()=>{
-        price_label.textContent += ` (avg: ${avg_price} zł)`;
+        price_label.textContent += ` (avg: ${avg_price} ${CURRENCY})`;
 
-        const span_percent = document.createElement('span');
-        const percents = Math.round((avg_price / original_price) * 100);
+        const span_to_gain = document.createElement('span');
+        span_to_gain.textContent = ` (${odds_to_gain}%)`;
+        span_to_gain.style.color = "green";
+        price_label.appendChild(span_to_gain);
 
-        span_percent.textContent = ` (${percents}%)`;
-        span_percent.style.color = percents > 100 ? "green" : "red";
+        const span_to_lose = document.createElement('span');
+        span_to_lose.textContent = ` (${odds_to_lose}%)`;
+        span_to_lose.style.color = "red";
+        price_label.appendChild(span_to_lose);
 
-        price_label.appendChild(span_percent);
     }, 1000);
 
 }
