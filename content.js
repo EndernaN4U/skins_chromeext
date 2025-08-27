dca25aa5688cbcb753aa004add4b8119f07758db
Enderna <adryanender@gmail.com>
2025-08-27 15:51:28 +0200

    Refactored case_skin code Added avg_price calcs into retrieve_case_skins function. Changed chances to array of price and odds

--- content.js
+++ content.js
@@ -6,29 +6,17 @@ const inject_content = ()=>{
     const original_price = parse_currency(price_label.textContent);
 
     let avg_case_gain = 0,
-        odds_to_gain = 0,   // Gain or make 0
-        odds_to_lose = 0;
+        odds_to_gain = 0,   // >= original price
+        odds_to_lose = 0;   // < original price
     
     skins.forEach(( skin ) => {
-        let prices_by_odds = 0,
-            sum_of_odds = 0;
+        skin.chances.forEach((chance) => {
 
-        skin.chances.forEach((chance_row) => {
-            let [_, price, __, odds] = [...chance_row.querySelectorAll('td')].map(child => child.textContent);
+            if(chance.price < original_price) odds_to_lose += chance.odds;
+            else odds_to_gain += chance.odds;
 
-            price = parse_currency(price);
-            odds = parseFloat(odds);
-
-            if(price < original_price) odds_to_lose += odds;
-            else odds_to_gain += odds;
-
-            prices_by_odds += price * odds;
-            sum_of_odds += odds;
-
-            avg_case_gain += price * odds;
+            avg_case_gain += chance.price * chance.odds;
         });
-
-        skin.avg_price = Math.round(prices_by_odds / sum_of_odds * 100) / 100;
 
         const gradient_color = skin.avg_price < original_price ? 'red' : 'green';
         skin.gradient.style.backgroundImage = `linear-gradient(180deg, transparent 0, ${gradient_color} 100%)`;
--- skins/case_skins.js
+++ skins/case_skins.js
@@ -3,13 +3,31 @@ const retrieve_case_skins = () => {
 
     return [...skins_containers].map((container) => {
         const gradient = container.querySelector(PAGE_CONFIG.skin_gradient);
-        const chances = container.querySelectorAll(PAGE_CONFIG.skin_chances);
+        const chances_table_row = container.querySelectorAll(PAGE_CONFIG.skin_chances);
+
+        let avg_price = 0,
+            price_by_odds = 0,
+            sum_of_odds = 0;
+
+        const chances = [...chances_table_row].map((chance_row)=>{
+            let [_, price, __, odds] = [...chance_row.querySelectorAll('td')].map(child => child.textContent);
+
+            price = parse_currency(price);
+            odds = parseFloat(odds);
+
+            price_by_odds += price * odds;
+            sum_of_odds += odds;
+
+            return { price, odds };
+        });
+
+        avg_price = Math.round(price_by_odds / sum_of_odds * 100) / 100;
 
         return {
             container,
             gradient,
             chances,
-            avg_price: 0
+            avg_price
         }
     });
 }
